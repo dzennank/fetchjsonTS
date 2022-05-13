36aa4551bc61f5f531f7a6beac2adcb80d973ad0
dzennank <dzenankrlic@gmail.com>
2022-05-13 15:27:26 +0200

    ...

--- index.ts
+++ index.ts
@@ -3,5 +3,12 @@ import axios from "axios";
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then(response => {
-    console.log(response.data);
+    const todo = response.data;
+
+    const Title = todo.title;
+    const ID = todo.id;
+    const isCompleted = todo.completed;
+
+    console.log(`The todo with id: ${ID} has a title of ${Title}
+    Is it finished? ${isCompleted}`)
 })
